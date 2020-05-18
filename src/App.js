0286b6c60ff23d69578d78bb46d0bb357a452583
Ryan Kloud <rkloud@catalyte.io>
2020-05-18 10:45:16 -0500

    finished lapcounter exercise

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class LapCounter extends React.Component {
+  constructor(props){
+    super(props);
+      this.state = {
+        lapCount : 0,
+        lapArray : []
+      }
+      this.addLaps = this.addLaps.bind(this);
+      this.decreaseLaps = this.decreaseLaps.bind(this);
+      this.resetLaps = this.resetLaps.bind(this);
+      this.saveLaps = this.saveLaps.bind(this);
+  }
+  addLaps(){
+    this.setState({
+      lapCount : this.state.lapCount + 1
+    });
+  }
+  decreaseLaps() {
+    if(this.state.lapCount > 0) {
+      this.setState({
+        lapCount : this.state.lapCount - 1
+      });
+    }
+  }
+  resetLaps() {
+    this.setState({
+      lapCount : 0
+    })
+  }
+  saveLaps() {
+    this.state.lapArray.push(this.state.lapCount);
+    this.setState({
+      lapCount : 0
+    })
+
+  }
+
+  render() {
+    return(
+      <div>
+        <h2>LapCounter</h2>
+        <p>Current Laps: {this.state.lapCount}</p>
+        <button onClick={this.addLaps}>Add Lap</button>
+        <button onClick={this.decreaseLaps}>Decrease Laps</button>
+        <button onClick={this.resetLaps}>Reset Count</button>
+        <button onClick={this.saveLaps}>Save Laps</button>
+        <SavedLaps lapArray={this.state.lapArray}/>
+      </div>
+    )
+  }
 }
 
-export default App;
+function SavedLaps(props)  {
+    let listLaps = props.lapArray.map((lap) => 
+    <li>{lap}</li>);
+    return(
+      <div>
+        <h3>Previous Laps</h3>
+        <ul>{listLaps}</ul>
+      </div>
+    )
+
+}
+
+
+
+export default LapCounter
